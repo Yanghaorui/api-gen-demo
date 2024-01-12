e4dcee3a3d6734344c047f23635fc69f932f839c
cnHaoYan <haorui.yang@lego.com>
2024-01-12 17:06:55 +0800

    init

---
+++ src/main/java/indi/haorui/apigendemo/ApiGenDemoApplication.java
@@ -0,0 +1,13 @@
+package indi.haorui.apigendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGenDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGenDemoApplication.class, args);
+    }
+
+}
